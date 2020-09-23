ced94b6532ae79ef8ea18ad75d717bbc8229b297
Ziyang Guo <guoziyang0033@gmail.com>
2020-09-23 15:52:03 +0800

    调整了初始化顺序

--- kernel/main.c
+++ kernel/main.c
@@ -1,28 +1,12 @@
 #include "types.h"
 #include "defs.h"
 
-void frame_alloc_test();
-
 void
 main()
 {
-    init_interrupt();
     init_memory();
-    frame_alloc_test();
+    init_interrupt();
     init_timer();
     asm volatile("ebreak");
     printf("return from trap!\n");
-}
-
-void
-frame_alloc_test()
-{
-    printf("alloc 0x%x\n", alloc());
-    uint64 f = alloc();
-    printf("alloc 0x%x\n", f);
-    printf("alloc 0x%x\n", alloc());
-    printf("dealloc 0x%x\n", f);
-    dealloc(f);
-    printf("alloc 0x%x\n", alloc());
-    printf("alloc 0x%x\n", alloc());
 }
