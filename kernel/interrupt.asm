436cc6de248b9ba5a64e9dda4ed5ca3825547dd8
Ziyang Guo <guoziyang0033@gmail.com>
2020-12-16 12:43:20 +0800

    修改中断切换，用于判断是否来自用户态

--- kernel/interrupt.asm
+++ kernel/interrupt.asm
@@ -29,14 +29,16 @@
     .balign 4
 # 全局中断处理，保存 Context 并跳转到 handleInterrupt() 处
 __interrupt:
+    csrrw   sp, sscratch, sp
+    bnez    sp, from_user
+from_kernel:
+    csrr    sp, sscratch
+from_user:
     # 移动栈指针，留出 Context 的空间
     addi    sp, sp, -34*REG_SIZE
     
     # 保存通用寄存器，其中 x0 固定为 0
     SAVE    x1, 1
-    # 将原来的 sp 写入 2 位置
-    addi    x1, sp, 34*REG_SIZE
-    SAVE    x1, 2
     # 循环保存 x3 至 x31
     .set    n, 3
     .rept   29
@@ -44,9 +46,13 @@ __interrupt:
         .set    n, n + 1
     .endr
 
+    csrrw   s0, sscratch, x0
     # 保存 CSR
     csrr    s1, sstatus
     csrr    s2, sepc
+
+    # 将 sp、sstatus 和 sepc 保存到栈上 
+    SAVE    s0, 2
     SAVE    s1, 32
     SAVE    s2, 33
 
@@ -57,7 +63,6 @@ __interrupt:
     csrr    a2, stval
     jal     handleInterrupt
 
-
     .globl __restore
 # 从 handleInterrupt 返回
 # 从 Context 中恢复所有寄存器，并跳转至 Context 中 sepc 的位置
@@ -65,9 +70,15 @@ __restore:
     # 恢复 CSR
     LOAD    s1, 32
     LOAD    s2, 33
+
+    andi    s0, s1, 1 << 8
+    bnez    s0, to_kernel
+to_user:
+    addi    s0, sp, 34 * REG_SIZE
+    csrw    sscratch, s0
+to_kernel:
     csrw    sstatus, s1
     csrw    sepc, s2
-
     # 恢复通用寄存器
     LOAD    x1, 1
     # 恢复 x3 至 x31
